9923476567bb92298c856e8254a36567230ca9e4
Melo-JP <63808642+Melo-JP@users.noreply.github.com>
2020-04-19 10:34:26 +0300

    Calculating frequency
    Calculate the frequency of the letters entered and then sort them from 'a' to 'z'

---
+++ FrequencyAnalysisCaesarCipher.java
@@ -0,0 +1,41 @@
+package com.paulmelvin;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FrequencyAnalysisCaesarCipher {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter message to be decrypted: ");
+        String encryptedText = input.nextLine().toLowerCase();
+
+        int[] freq = new int[encryptedText.length()];
+        int i;
+        int j;
+
+        //Converts given string into character array
+        char string[] = encryptedText.toCharArray();
+
+        for(i = 0; i <encryptedText.length(); i++) {
+
+            freq[i] = 1;
+            for(j = i+1; j <encryptedText.length(); j++) {
+                if(string[i] == string[j]) {
+                    freq[i]++;
+
+                    //Set string[j] to 0 to avoid printing visited character
+                    string[j] = '0';
+                 Arrays.sort(string);
+                }
+            }
+        }
+
+        //Displays each character and their corresponding frequency
+        System.out.println("Characters and their corresponding frequencies");
+        for(i = 0; i <freq.length; i++) {
+            if(string[i] != ' ' && string[i] != '0')
+                System.out.println(string[i] + "-" + freq[i]);
+
+        }
+    }
+}
